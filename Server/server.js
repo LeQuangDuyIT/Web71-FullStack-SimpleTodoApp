21b780234be11e2c8464c7c354fb5cb1eacc5484
lqduycp <lqduycp@gmail.com>
2023-09-25 00:41:22 +0700

    Add Cors whitelist

--- Server/server.js
+++ Server/server.js
@@ -3,13 +3,25 @@ import cors from 'cors';
 import appRouter from './routes/index.js';
 import { connectToDatabase } from './config/database.js';
 
+const whitelist = ['http://localhost:5173'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: 'PUT,PATCH,GET,DELETE,UPDATE'
+};
+
 const app = express();
 const PORT = 3001;
 
 connectToDatabase();
 
 app.use(express.json());
-app.use(cors('*'));
+app.use(cors(corsOptions));
 
 app.use('/api/v1', appRouter);
 
